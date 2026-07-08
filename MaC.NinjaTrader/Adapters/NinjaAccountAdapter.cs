c8c77ff309dc6520f43c28f215f347f5f6f5b94d
Andres Agudelo <dililamb95@gmail.com>
2026-07-08 18:19:13 -0400

    Add NinjaTrader account adapter

---
+++ MaC.NinjaTrader/Adapters/NinjaAccountAdapter.cs
@@ -0,0 +1,23 @@
+﻿using MaC.Core.Factories;
+using MaC.Core.Models;
+using MaC.Core.Results;
+using MaC.Core.Services;
+using MaC.NinjaTrader.Mappers;
+
+namespace MaC.NinjaTrader.Adapters;
+
+public class NinjaAccountAdapter
+{
+    private readonly NinjaTradeMapper _tradeMapper = new();
+    private readonly AccountEngine _engine = AccountEngineFactory.Create();
+
+    public EngineResult Process(
+        TradingContext context,
+        decimal netPnL,
+        int contracts)
+    {
+        context.Trade = _tradeMapper.Map(netPnL, contracts);
+
+        return _engine.ProcessTrade(context);
+    }
+}
